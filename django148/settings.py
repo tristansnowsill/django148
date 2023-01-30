56f6be313e99070aa48172c09c98546e8dd7e387
Tristan Snowsill <tristan.snowsill@googlemail.com>
2023-01-30 11:15:14 +0000

    Remove SECRET_KEY from version control

--- django148/settings.py
+++ django148/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xj_*5by)o-)tgh)wj1=v64qdmc3xs*it5#uk@9^6qu$%tlu)@b"
+SECRET_KEY = os.environ[ 'SECRET_KEY' ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
